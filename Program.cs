d6d22986c02a7bc833e2487f4ed7d732bd084d16
Tobe-Deleted <oso082024@gmail.com>
2024-10-17 21:00:15 +0200

    Improved flow

--- Program.cs
+++ Program.cs
@@ -4,56 +4,81 @@
     {
         static void Main(string[] args)
         {
+            
+            Calculator calculator = new Calculator();
             string? value;
             
                 do
                 {
-            
-                
+                    bool ValidNumber = false;
+                    string? CalculationMethod = "0";
+                    double NumberA = double.NaN;
+                    double NumberB = 0;
+                    
+                    while (!ValidNumber)
+                    {
+                        
+                        try
+                        {
+                            Console.Write("Enter first number:");
+                            NumberA = Convert.ToDouble(Console.ReadLine());
+                            ValidNumber = true;
+                        }
+                        catch(FormatException)
+                        {
+                            Console.WriteLine("input must be a number");
+                            ValidNumber = false;
+                        }
+                    }
+                    
+                    while (ValidNumber)
+                    {
+                        Console.Write("Enter operation(+, -, *, / or ^):");
+                        CalculationMethod = Console.ReadLine();
+                        if (CalculationMethod == "+" || CalculationMethod == "-" || CalculationMethod == "*" || CalculationMethod == "/" || CalculationMethod == "^")
+                        ValidNumber = false;
+                    }
+                    
+                    while (!ValidNumber)
+                    {
+                        
+                        try
+                        {
+                            Console.Write("Enter second number:");
+                            NumberB = Convert.ToDouble(Console.ReadLine());
+                            ValidNumber = true;
+                        }
+                        catch(FormatException)
+                        {
+                            Console.WriteLine("input must be a number");
+                            ValidNumber = false;
+                        }
+                    }
 
-                    try{
-                        double Results;
-                    
-                        Console.Write("Enter first number:");
-                        double NumberA = Convert.ToDouble(Console.ReadLine());
-                        Console.Write("Enter method(+, -, *, / or ^):");
-                        string? CalculationMethod = Console.ReadLine();
-                        Console.Write("Enter second number:");
-                        double NumberB = Convert.ToDouble(Console.ReadLine());
-                    
-    
                         switch (CalculationMethod)
                         {
                             case "+":
-                                Results = NumberA + NumberB;
-                                Console.WriteLine(NumberA + CalculationMethod + NumberB + "=" + Results);
+                                Console.WriteLine($"{NumberA} + {NumberB} = {calculator.AddNumbers(NumberA, NumberB)}");
                                 break;
                             case "-":
-                                Results = NumberA - NumberB;
-                                Console.WriteLine(NumberA + CalculationMethod + NumberB + "=" + Results);
+                                Console.WriteLine($"{NumberA} - {NumberB} = {calculator.SubtractNumbers(NumberA, NumberB)}");
                                 break;
                             case "*":
-                                Results = NumberA * NumberB;
-                                Console.WriteLine(NumberA + CalculationMethod + NumberB + "=" + Results);
+                                Console.WriteLine($"{NumberA} * {NumberB} = {calculator.MultiplyNumbers(NumberA, NumberB)}");
                                 break;
                             case "/":
-                                Results = NumberA / NumberB;
-                                Console.WriteLine(NumberA + CalculationMethod + NumberB + "=" + Results);
+                                Console.WriteLine($"{NumberA} / {NumberB} = {calculator.DivideNumbers(NumberA, NumberB)}");
                                 break;
                             case "^":
-                                Results = Math.Pow(NumberA, NumberB);
-                                Console.WriteLine(NumberA + CalculationMethod + NumberB + "=" + Results);
+                                Console.WriteLine($"{NumberA} ^ {NumberB} = {calculator.Potency(NumberA, NumberB)}");
                                 break;
 
                             default:
-                                Console.WriteLine("incorrect input for method, use +, -, *, / or ^");
+                                Console.Write("operation is not +, -, *, / or ^ :");
                                 break;
                         }
-                    }
-                    catch(FormatException)
-                    {
-                        Console.WriteLine("input must be a number");
-                    }
+                    
+
                         Console.Write("Do you want to continue(y/n):");
                         value = Console.ReadLine();
                 }
--- obj/Debug/net8.0/csharpcalculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharpcalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharpcalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3626df02faa1db25cfa9f280d075f0497603f3d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c6cd261c89b55ccf6ddddc7a1ff1a466bc40de")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharpcalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharpcalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
